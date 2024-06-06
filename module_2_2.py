4678ee7d5d039b7b07b0a6e62539e793ab62e376
Igor949 <igor-syachin@mail.ru>
2024-06-06 22:10:21 +0300

    Add files via upload
    Домашняя работа "Домашняя работа по уроку "Условная конструкция. Операторы if, elif, else."

---
+++ module_2_2.py
@@ -0,0 +1,12 @@
+print('введите три целых числа:')
+first=int(input())
+print(first)
+second=int(input())
+print(second)
+third=int(input())
+if first==second==third:
+    print(int(3))
+elif first==third:
+      print(int(2))
+else:
+    print(int(0))
