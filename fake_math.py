21dc08bcc6ce53b5b7b21a38034eda9e3b7e00c6
Igor949 <igor-syachin@mail.ru>
2024-09-12 20:09:01 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,13 @@
+from math import inf
+def divide(first , second):
+    if second == 0:
+        return ('Ошибка')
+
+    else:
+        result = first / second
+        return result
+
+
+
+
+divide(8,2)
