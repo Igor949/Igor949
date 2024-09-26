5efa79e79959fbb2c8a0d8cbb3b48e864aad37bc
Igor949 <igor-syachin@mail.ru>
2024-09-26 15:34:14 +0300

    Add files via upload

--- 5.3.py
+++ 5.3.py
@@ -17,36 +17,51 @@ class House():
         return (f"Название: {self.name}, кол-во этажей: {self.number_of_floors}")
 
     def __eq__(self, other):
-        return self.name == other
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors == other
 
-    def __lt__(self,other):
-        return self.name < other.name
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        elif isinstance(other, int):
+            return self.number_of_floors < other
 
-    def __le__(self,other):
-        return self.name <= other.name
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.__eq__(other) or self.__lt__(other)
+
 
     def __gt__(self,other):
-        return self.name > other.name
+        return not self.__le__(other)
 
     def __ge__(self,other):
-        return self.name >= other.name
+        return not self.__lt__(other)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
 
-    def __add__(self, value):
-        return self.number_of_floors + value
+    def __add__(self, value):  # h1 = h1 + 10 или h1 = h1 + h2
+        if isinstance(value, int):
+            self.number_of_floors += value
+        elif isinstance(value, House):
+            self.number_of_floors += value.number_of_floors
+        return self
 
-    def __radd__(self, value):
-        return self.number_of_floors + value
+    def __radd__(self, other):
+        return self.__add__(other)
 
-    def __iadd__(self,value):
-        return self.number_of_floors + value
+    def __iadd__(self, other: int):
+        return self.__add__(other)
 
 
 
 h1 = House('ЖК Горский', 10)
-h2 = House('Домик в деревне', 20)
-h1.go_to(25)
-h2.go_to(7)
+h2 = House('Домик в деревне', 12)
+h1.go_to(10)
+h2.go_to(2)
 # __len__
 print(len(h1))
 print(len(h2))
@@ -54,26 +69,26 @@ print(len(h2))
 print(h1)
 print(h2)
 # __eq__
-print(h1 == h2)
+print(h1 == h2, "__eq__")
 # __lt__
-print(h1 < h2)
+print(h1 < h2, "__lt__")
 # __le__
-print(h1 <= h2)
+print(h1 <= h2, "__le__")
 # __gt__
-print(h1 > h2)
+print(h1 > h2, "__gt__")
 # __ge__
-print(h1 >= h2)
+print(h1 >= h2, "__ge__")
 # __ne__
-print(h1 != h2)
+print(h1 != h2, "__ne__")
 # __add__
 h1 = h1 + 10
-print(h1)
-print(h1 == h2)
+print(h1, "__add__")
+print(h1 == h2,"__add__")
 #__rad__
 h1 = h1 + 5
-print(h1)
-print(h1 == h2)
+print(h1,"__rad__")
+print(h1 == h2, "__rad__")
 #_iadd__
 h1 = h1 + 1
-print(h1)
-print(h1 != h2)
+print(h1, "__iadd")
+print(h1 != h2, "__iadd__")
